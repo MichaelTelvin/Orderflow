120d00d0e958f31744264623eaee28b12a775913
Michael Telvin <yugmtp@gmail.com>
2026-06-03 17:52:00 +0400

    add backend health endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,3 +10,16 @@ fastify.get("/health", async () => {
     };
 });
 
+const start = async () => {
+    try {
+        await fastify.listen({
+            port: 3000,
+            host: "0.0.0.0",
+        });
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
